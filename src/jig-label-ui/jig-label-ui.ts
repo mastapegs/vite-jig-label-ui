664984052114c5ef1eeb25487e292d1b643ffb9f
Matthew Pagan <mastapegs01@gmail.com>
2021-12-02 20:09:04 -0500

    fix: remove unneeded commented code

--- src/jig-label-ui/jig-label-ui.ts
+++ src/jig-label-ui/jig-label-ui.ts
@@ -44,16 +44,3 @@ export class JigLabelUI extends LitElement {
     `;
   }
 }
-
-// ${this.jigs.map((jig) => {
-//   return html`
-//     <single-jig>
-//       <span slot="jig-name">${jig.jig_name}</span>
-//       <span slot="author">${jig.author}</span>
-//       <span slot="author-badge">${jig.author_badge}</span>
-//       <span slot="date">${jig.date}</span>
-//       <span slot="language">${jig.language}</span>
-//       <span slot="curators">${jig.curators}</span>
-//     </single-jig>
-//   `;
-// })}
