da4d98ba0287ba17e16b4dcae233bca8186f30c1
Matthew Pagan <mastapegs01@gmail.com>
2021-12-02 19:26:21 -0500

    feat: extract everything out of lit properties

--- src/jig-label-ui/jig-label-ui.stories.ts
+++ src/jig-label-ui/jig-label-ui.stories.ts
@@ -1,18 +1,33 @@
 import { Meta, Story } from "@storybook/web-components";
 import { html } from "lit";
 import { JigData } from "./types";
-import { JigLabelUI } from "./jig-label-ui";
 import { jigs } from "./data";
 
-import './jig-label-ui'
+import "./jig-label-ui";
+import "../single-jig";
 
 export default {
   title: "Jig Label UI",
-  component: 'jig-label-ui',
+  component: "jig-label-ui",
 } as Meta;
 
-const Template: Story<JigLabelUI> = ({ jigs }: { jigs: JigData[] }) =>
-  html`<jig-label-ui .jigs="${jigs}"></jig-label-ui>`;
+const Template: Story = ({ jigs }) =>
+  html`
+    <jig-label-ui>
+      ${jigs.map(
+        (jig: JigData) => html`
+          <single-jig>
+            <span slot="jig-name">${jig.jig_name}</span>
+            <span slot="author">${jig.author}</span>
+            <span slot="author-badge">${jig.author_badge}</span>
+            <span slot="date">${jig.date}</span>
+            <span slot="language">${jig.language}</span>
+            <span slot="curators">${jig.curators}</span>
+          </single-jig>
+        `
+      )}
+    </jig-label-ui>
+  `;
 
 export const Primary = Template.bind({});
 Primary.args = {
--- src/jig-label-ui/jig-label-ui.ts
+++ src/jig-label-ui/jig-label-ui.ts
@@ -1,16 +1,6 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import jigFlex from "../common-css/jig-flex-css";
-import "../single-jig";
-
-export interface JigData {
-  jig_name: string;
-  author: string;
-  author_badge: string;
-  date: string;
-  language: string;
-  curators: string;
-}
 
 @customElement("jig-label-ui")
 export class JigLabelUI extends LitElement {
@@ -42,9 +32,6 @@ export class JigLabelUI extends LitElement {
     "Curators",
   ];
 
-  @property({ attribute: false })
-  jigs: JigData[] = [];
-
   render() {
     return html`
       <div class="container">
@@ -53,19 +40,21 @@ export class JigLabelUI extends LitElement {
             (header) => html`<div class="flex">${header}</div>`
           )}
         </div>
-        ${this.jigs.map((jig) => {
-          return html`
-            <single-jig>
-              <span slot="jig-name">${jig.jig_name}</span>
-              <span slot="author">${jig.author}</span>
-              <span slot="author-badge">${jig.author_badge}</span>
-              <span slot="date">${jig.date}</span>
-              <span slot="language">${jig.language}</span>
-              <span slot="curators">${jig.curators}</span>
-            </single-jig>
-          `;
-        })}
+        <slot></slot>
       </div>
     `;
   }
 }
+
+// ${this.jigs.map((jig) => {
+//   return html`
+//     <single-jig>
+//       <span slot="jig-name">${jig.jig_name}</span>
+//       <span slot="author">${jig.author}</span>
+//       <span slot="author-badge">${jig.author_badge}</span>
+//       <span slot="date">${jig.date}</span>
+//       <span slot="language">${jig.language}</span>
+//       <span slot="curators">${jig.curators}</span>
+//     </single-jig>
+//   `;
+// })}
